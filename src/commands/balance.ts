f6f813ee9f57d8043048ceb4a7598fd3cf57eb9c
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2023-04-25 08:36:39 -0400

    put record when not found

--- src/commands/balance.ts
+++ src/commands/balance.ts
@@ -1,5 +1,6 @@
 import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js'
 import { Level } from 'level'
+import { UserRecord } from '../types.js'
 
 const description = new SlashCommandBuilder()
   .setName('balance')
@@ -7,14 +8,17 @@ const description = new SlashCommandBuilder()
 
 const execute = async (interaction: ChatInputCommandInteraction, db: Level<string, object>) => {
   try {
-    const record = (await db.get(interaction.user.username)) as any
+    const record = (await db.get(interaction.user.username)) as UserRecord
     if (record.balance !== undefined) {
-      await interaction.reply(`Your balance is ${record.balance} $MOON`)
+      await interaction.reply({ content: `Your balance is ${record.balance} $MOON`, ephemeral: true})
     } else {
-      await interaction.reply(`Your balance is 0 $MOON`)
+      await interaction.reply({ content: `Your balance is 0 $MOON`, ephemeral: true})
     }
   } catch (err: any) {
-    if (err.code === 'LEVEL_NOT_FOUND') await interaction.reply(`Your balance is 0 $MOON`) 
+    if (err.code === 'LEVEL_NOT_FOUND') {
+      await interaction.reply(`Your balance is 0 $MOON`) 
+      await db.put(interaction.user.username, { balance: '0', twitter: ''})
+    }
     else await interaction.reply(`Something went wrong.  Please try again later`)
   }
 }
